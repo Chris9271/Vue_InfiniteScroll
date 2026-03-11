8db5df4fb28e7e9d2537e72a8af78f791ff62f7e
Chris <chrisweng18@gmail.com>
2026-03-11 11:40:50 +0800

    v0.05 feat: 新增觸發加載邏輯＆觸發該邏輯的元素

--- src/views/Repository.vue
+++ src/views/Repository.vue
@@ -1,20 +1,25 @@
-<script setup></script>
 <script setup>
 import RepoCard from '@/components/RepoCard.vue'
 import { ref, onMounted, onBeforeMount } from 'vue'
 import axios from 'axios'
 import { useAppStore } from '@/stores/appStore'
 
-<template></template>
+// 禁用瀏覽器滾動恢復
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+
 // 頁面上顯示的Repo清單
 const reposData = ref([])
 
-<style lang="scss" scoped></style>
 // 頁面上已載入的資料長度
 const totalLoadedCount = ref(0)
 
 // 頁碼(打API用)
 const page = ref(1)
+
+// 觸發加載資料的元素
+const triggerElement = ref(null)
 
 const appStore = useAppStore()
 
@@ -36,16 +41,35 @@ const getReposData = async (dataCount) => {
     appStore.asyncCloseLoader()
   }
 }
+
+onBeforeMount(() => {
+  window.scrollTo(0, 0)
+})
+
 onMounted(async () => {
   // 取得repo的資料(初次讀取至少30筆)
   await getReposData(30)
 
+  // 建立一個新的IntersectionObserver物件，當監聽到目標元素的可見部分超過特定值時，會執行特定的callback function
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        // 每次載入再取得10筆
+        getReposData(10)
+      }
+    },
+    { threshold: 0.5 }
+  )
+
+  observer.observe(triggerElement.value)
 })
 </script>
+
 <template>
   <div class="list">
     <h2>Data source: GitHub repos of facebook</h2>
     <RepoCard :dataList="reposData" />
+    <div ref="triggerElement"></div>
   </div>
 </template>
 
